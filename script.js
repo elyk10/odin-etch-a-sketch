76eda6de11df9733f62ee5b0efa073242038760b
elyk10 <144371327+elyk10@users.noreply.github.com>
2025-08-21 13:52:41 -0400

    created grid with flexbox
    used flexbox css to create the 16 x 16 grid of divs

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const container = document.querySelector(".container");
+
+    for (let i = 0; i < 16; i++)
+    {
+        for (let j = 0; j < 16; j++)
+        {
+            let child = document.createElement("div");
+            child.textContent = "Div " + i + " " + j;
+            child.setAttribute("style", "border: 2px solid black; flex: 1 1 calc(6.25% - 4px);");
+            container.appendChild(child);
+        }
+    }
+});
