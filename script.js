b0844928ad030b80a21ad35f2d58c39c83241e87
elyk10 <144371327+elyk10@users.noreply.github.com>
2025-08-21 18:19:48 -0400

    added "Sketch" feature
    added hover event so when div has mouse hover over it it will change colour

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
         for (let j = 0; j < 16; j++)
         {
             let child = document.createElement("div");
+            child.classList.add("block");
             child.textContent = "Div " + i + " " + j;
             child.setAttribute("style", "border: 2px solid black; flex: 1 1 calc(6.25% - 4px);");
+            child.addEventListener("mouseover", () => {
+                child.classList.add("hover");
+            })
+
             container.appendChild(child);
         }
     }
+
 });
