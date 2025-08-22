e0e45992db1ef0519e1322bf1edd22dceb71197a
elyk10 <144371327+elyk10@users.noreply.github.com>
2025-08-22 13:41:28 -0400

    added size change button
    button to change size of the grid from 1 to 100

--- script.js
+++ script.js
@@ -2,20 +2,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const container = document.querySelector(".container");
 
-    for (let i = 0; i < 16; i++)
+    createGrid(container);
+
+    let button = document.querySelector(".size");
+
+    button.addEventListener("click", () => {
+        let gridSize = parseInt(prompt("What size grid would you like?", "Default size = 16"));
+        if (gridSize > 100)
+            gridSize = 100;
+        else if (gridSize < 1)
+            gridSize = 1;
+
+        container.replaceChildren();
+        
+        /*while (container.firstChild)
+        {
+            container.removeChild(container.lastChild);
+        }
+*/
+        createGrid(container, gridSize);
+    });
+
+});
+
+
+function createGrid(container, size = 4)
+{
+    for (let i = 0; i < size; i++)
     {
-        for (let j = 0; j < 16; j++)
+        for (let j = 0; j < size; j++)
         {
             let child = document.createElement("div");
             child.classList.add("block");
-            child.textContent = "Div " + i + " " + j;
-            child.setAttribute("style", "border: 2px solid black; flex: 1 1 calc(6.25% - 4px);");
+            //child.textContent = "Div " + i + " " + j;
+            
+            let num = 100/size;
+            let height = window.innerHeight / size;
+            let style = "flex: 1 1 calc(" + num + "% - 4px); height: calc(" + num + "% - 4px);";
+            child.setAttribute("style", style);
+            // adds class that will change the background colour when mouse enters div
             child.addEventListener("mouseover", () => {
                 child.classList.add("hover");
-            })
+            });
 
             container.appendChild(child);
         }
     }
-
-});
+}
